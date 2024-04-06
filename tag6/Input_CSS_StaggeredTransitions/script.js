4f416abcb53844276786ec24f07b3686b2324e4c
Skuyngel <151165868+Skuyngel@users.noreply.github.com>
2024-04-06 14:43:49 +0200

    Add files via upload

---
+++ tag6/Input_CSS_StaggeredTransitions/script.js
@@ -0,0 +1,15 @@
+document.getElementById("changeToRed").addEventListener("click", function() {
+    document.documentElement.style.setProperty("--dominocolor", "firebrick");
+});
+document.getElementById("changeToYellow").addEventListener("click", function() {
+    document.documentElement.style.setProperty("--dominocolor", "goldenrod");
+});
+document.getElementById("changeToGreen").addEventListener("click", function() {
+    document.documentElement.style.setProperty("--dominocolor", "green");
+});
+document.getElementById("changeToBlue").addEventListener("click", function() {
+    document.documentElement.style.setProperty("--dominocolor", "dodgerblue");
+});
+document.getElementById("changeToViolet").addEventListener("click", function() {
+    document.documentElement.style.setProperty("--dominocolor", "mediumorchid");
+});
